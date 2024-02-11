aa5e365e93cff8d056ca69c10e3416f7617260b9
Carla Mendes <carlarsmendes@gmail.com>
2024-02-11 21:06:09 +0000

    VP-27 Styling header and steps sections; Cards steps & testimonials added.

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,6 +1,9 @@
  // Data for the cards
  const cardData = [
-    { name: 'Maria', jobName:'Designer Gráfica', imgUrl: './img/testimonial-1.png',  testimonial: `"A planilha de precificação foi um divisor de águas na minha carreira de designer. Finalmente, consigo compreender e valorizar meu trabalho de maneira justa. Uma ferramenta indispensável para todos os colegas de profissão!"` },
+    {   name: 'Maria', 
+        jobName:'Designer Gráfica', 
+        imgUrl: './img/testimonial-1.png',  
+        testimonial: `"A planilha de precificação foi um divisor de águas na minha carreira de designer. Finalmente, consigo compreender e valorizar meu trabalho de maneira justa. Uma ferramenta indispensável para todos os colegas de profissão!"` },
     { name: 'João', jobName:'Estudante de Design', imgUrl: './img/testimonial-1.png',  testimonial: `""Como designer iniciante, a planilha foi um guia essencial para entender como precificar meus projetos. Agora, me sinto mais confiante e valorizo meu trabalho de forma justa. Obrigado por apoiar os designers em início de carreira!"` },
     { name: 'Carlos', jobName:'Designer de Produto', imgUrl: './img/testimonial-1.png',  testimonial: `"A iniciativa em compartilhar sua planilha de precificação é louvável. Essa ferramenta não apenas descomplicou o processo de formação de preço, mas também reforçou a importância e o valor do design como arte e ciência. Uma contribuição valiosa para a comunidade de designers!"` }
 ];
@@ -8,7 +11,7 @@
 // Function to create a card element
 function createCard(name, content) {
     const card = document.createElement('div');
-    card.classList.add('c-testimonial-card');
+    card.classList.add('c-card');
 
     const cardTitle = document.createElement('h2');
     cardTitle.textContent = name;
